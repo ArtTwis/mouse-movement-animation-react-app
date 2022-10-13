e8bb0f819189794a1ccd1c83dda158d54728c3bd
Twinkle Goyal <twinkle.goyal@intimetec.com>
2022-10-14 01:00:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import useScript from './useScript';
 
-function App() {
+const App = ({ url }) => {
+  useScript(url);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <canvas id='canvas' />
     </div>
   );
-}
+};
 
 export default App;
